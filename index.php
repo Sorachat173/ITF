39b1334cdba85ad9f5c3777058c2007092353f46
Sorachat173 <73683414+Sorachat173@users.noreply.github.com>
2020-10-30 15:38:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,11 @@
         <h2>My dog</h2>
         <img src="download.jpg">
         <a href="https://www.it.kmitl.ac.th/th/">Click this Link</a>
-     
+        <ul>
+            <li>A:วันก่อนครับ</li>
+            <li>ฺB:วันนี้อะ</li>
+            <li>A:วันนี้หลวม</li>
+            <h5>ครับ = คับ นะครับ</h>
+        </ul>
     </body>
 </html>
