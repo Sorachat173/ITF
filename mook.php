dad5db95f253ce63edefd5d3dce14dddb7c1b720
Sorachat173 <73683414+Sorachat173@users.noreply.github.com>
2020-10-30 15:22:01 +0700

    Add files via upload

---
+++ mook.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>mook</title>
+    </head>
+
+    <body>
+        <h1>This is My mook</h1>
+        <p>Sorachat Sungkhapan</p>
+        <p>Ter</p>
+        <h2>My cat</h2>
+        <img src="dog.jpg">
+        <h2>My dog</h2>
+        <img src="download.jpg">
+        <a href="https://www.it.kmitl.ac.th/th/">Click this Link</a>
+    </body>
+</html>
