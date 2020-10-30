5b2b1f1f0cf66bd83282b709c3638eaed9a0a101
Sorachat173 <73683414+Sorachat173@users.noreply.github.com>
2020-10-30 14:55:58 +0700

    commit ไง

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>Hello world</title>
+    </head>
+
+    <body>
+        <h1>This is Greek</h1>
+        <p>Shi is cat</p>
+        <img src="greek.jpg">
+        <br>
+
+        <h2>This is Link to go to Hololive Channels</h2>
+        <a href="https://www.it.kmitl.ac.th/th/">Click this Link</a>
+
+        <h2>Under list</h2>
+        <ul>
+            <li>Google</li>
+            <li>Google</li>
+            <li>Google</li>
+        </ul>
+    </body>
+</html>
